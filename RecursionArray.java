8c38148a1dc1ba624517481808133de0cb2c3111
Derin Gbadebo <daireenonline@yahoo.com>
2019-02-12 22:27:27 -0800

    Calculating the sum of an array via recursion

---
+++ RecursionArray.java
@@ -0,0 +1,20 @@
+public class RecursionArray {
+	
+//	Calculating the sum of an array via recursion
+	
+	public static void main(String[] args) {
+		int[] givenArray = {1,2,3,4,5,6,7,8,9};
+		int arrayLength = givenArray.length;
+		int summation = arrayAdder(givenArray, arrayLength);
+		System.out.println( "The summation of the array by recursion is: " + summation );
+	}
+	
+//	tail recursion
+	public static int arrayAdder(int[] arr, int arrSize) {
+		if (arrSize == 1)
+			return arr[0];
+		else
+			return arr[arrSize-1] += arrayAdder(arr, arrSize - 1);
+	}
+
+}
