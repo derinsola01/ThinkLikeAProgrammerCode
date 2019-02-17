d724b9cf9cfb0e992def4faecf2de35b4e460811
Derin Gbadebo <daireenonline@yahoo.com>
2019-02-17 06:40:18 -0800

    Made the program modular

--- CheckStatisticalMode.java
+++ CheckStatisticalMode.java
@@ -2,20 +2,25 @@
 public class CheckStatisticalMode {
 
 	public static void main(String[] args) {
-		int[] numbers = {12, 16, 13, 16, 14, 16, 15, 17, 15, 16};
+		int[] numbers = {12, 16, 13, 20, 20, 20, 16, 14, 16, 15, 20, 20, 17, 15, 16, 20};
+		int modeNum = getMode(numbers);
+		System.out.println("modeNum is: " + modeNum);
+	}
+	
+	public static int getMode(int[] arr) {
 		int modeCounter = 0;
 		int elementVal = 0;
-		for (int i = 0; i < numbers.length; i++ ) {
+		for (int i = 0; i < arr.length; i++ ) {
 			int temp = 1;
-			for (int j = i + 1; j < numbers.length; j++ ) {
-				if(numbers[i] == numbers[j])
+			for (int j = i + 1; j < arr.length; j++ ) {
+				if(arr[i] == arr[j])
 					temp++;
 			}
 			if (temp > modeCounter) {
 				modeCounter = temp;
-				elementVal = numbers[i];
+				elementVal = arr[i];
 			}
 		}
-		System.out.println("modeCounter is: " + modeCounter + "\t elementVal is: " + elementVal);
+		return elementVal;
 	}
 }
