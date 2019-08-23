a57bc1866d763f1d5295e51787ec4b8d9708e66b
Derin Gbadebo <daireenonline@yahoo.com>
2019-08-23 04:38:27 -0400

    Update RecursionTest.java
    Bug fix.

--- RecursionTest.java
+++ RecursionTest.java
@@ -5,7 +5,7 @@ public class RecursionTest {
 	}
 	
 	public static int factorial (int number) {
-		if (number == 1)
+		if (number <= 1)
 			return 1;
 		else
 			return number * factorial(number - 1);		
